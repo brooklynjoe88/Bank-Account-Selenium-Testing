e8b13eafb77e2b7a7a60ac094a9c61331c431e88
brooklynjoe88 <60209190+brooklynjoe88@users.noreply.github.com>
2021-07-30 01:14:51 -0400

    Selenium Reworked
    Removed classes
    Adopted Page Object Model
    Added TestNG
    Moved ChromeDriver to project resource folder

---
+++ src/base/driver.java
@@ -0,0 +1,34 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import pages.HomePage;
+
+
+public class driver 
+{
+	
+	private WebDriver driver; 
+	protected HomePage homePage;
+	
+	@BeforeClass
+	public void setup()
+	{
+		System.setProperty("webdriver.chrome.driver", "Resources/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://parabank.parasoft.com/");
+		
+		homePage = new HomePage(driver);
+	}
+
+	//@AfterClass
+	public void tearDown()
+	{
+		driver.quit();
+	}
+	
+}
+ 
